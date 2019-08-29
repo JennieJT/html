1543e63269d60ba48d431b8a72882a9703316eb4
janejingtian <jingtianwang@jingtiaangdembp.home>
2019-08-29 08:28:31 -0400

    我讨厌table

--- exm3/examDlg.js
+++ exm3/examDlg.js
@@ -9,13 +9,5 @@ require(['WJTDlg'],function(Dlg){
     {
         document.getElementById('openbutton').addEventListener("click", popDialog, false);
     }
-    onload();
-    // if (document.all) {
-    //     window.attachEvent("onload", onload)//对于IE
-    //   }
-    //   else {
-    //       if(document.readyState=='')
-    //     window.addEventListener("load", onload, false);//对于FireFox
-    //   }
-  
+    onload(); 
  })
